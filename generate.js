e363a33cf23d157aa310713397c125a04c84c683
jonschlinkert <jon.schlinkert@sellside.com>
2015-11-27 04:19:47 -0500

    use question-store, cleanup

--- generate.js
+++ generate.js
@@ -1,54 +1,83 @@
 'use strict';
 
 var path = require('path');
-var through = require('through2');
-var extend = require('extend-shallow');
-var Questions = require('question-store');
 var argv = require('minimist')(process.argv.slice(2));
-var wrap = require('word-wrap');
-var set = require('set-value');
-var chalk = require('chalk');
+var utils = require('./utils');
 
 module.exports = function(app, base, env) {
-  var questions = new Questions();
   var dest = argv.dest || process.cwd();
+  var questions = utils.questions(argv);
+
+  /**
+   * Questions (answers are used to populate templates)
+   */
 
   questions
-    .set('name', 'What is the project name?')
-    .set('author.name', 'Author name?')
-    .set('author.url', 'Author URL?')
-    .set('username', 'Author username?')
-    .set('description', 'What is the project description?')
-    .set('varname', 'varname')
-    .set('year', 'What year is it?')
+    .setDefault('author.name', 'Author\'s name?')
+    .setDefault('author.url', 'Author\'s URL?')
+    .setDefault('author.username', 'Author\'s GitHub username?');
 
-  app.task('copy', function(cb) {
+  questions
+    .set('name', 'What is the project name?', {
+      default: utils.projectName(process.cwd()),
+      force: argv.init
+    })
+    .set('description', 'Project description?', {
+      default: 'My amazing node.js project.',
+      force: argv.init
+    });
+
+  /**
+   * Tasks
+   */
+
+  app.task('files', function(cb) {
+    intro(argv);
+
     questions.ask(function(err, answers) {
-      answers = toObject(answers);
+      if (err) return cb(err);
+      answers.varname = utils.namify(answers.name);
 
       app.toStream('templates')
-        .pipe(through.obj(function(file, enc, next) {
-          file.base = dest;
-          file.path = path.join(dest, file.basename);
-          file.data = extend({}, file.data, answers);
-          next(null, file);
-        }))
+        .pipe(getAnswers(dest, answers))
         .pipe(app.renderFile('text'))
-        .pipe(app.dest(rename({dest: dest})))
+        .pipe(app.dest(rename({
+          dest: dest
+        })))
         .on('error', cb)
         .on('finish', cb);
     });
   });
 
+  /**
+   * Prompt the user to run the `git` generator
+   */
+
   app.task('git', function(cb) {
-    base.generate('git', function(err) {
-      if (err) return cb(err);
-      cb();
-    });
+    base.runGenerators('git', cb);
   });
 
-  app.task('default', ['copy', 'git']);
+  app.task('default', ['files', 'git']);
 };
+
+/**
+ * Pass answers to templates as context
+ */
+
+function getAnswers(dest, answers) {
+  answers = expandProps(answers);
+
+  return utils.through.obj(function(file, enc, next) {
+    file.base = dest;
+    file.path = path.join(dest, file.basename);
+    file.data = utils.extend({}, file.data, answers);
+    next(null, file);
+  });
+}
+
+/**
+ * Rename template files
+ */
 
 function rename(options) {
   options = options || {};
@@ -59,18 +88,27 @@ function rename(options) {
   };
 }
 
-function toObject(obj) {
+/**
+ * Expand dot-notation in object paths.
+ *
+ * @param {Object} obj
+ * @return {Object}
+ */
+
+function expandProps(obj) {
   var data = {};
   for (var key in obj) {
-    var val = obj[key];
-    set(data, key, val);
+    utils.set(data, key, obj[key]);
   }
   return data;
 }
 
 function intro() {
-  console.log(chalk.bold('  Head\'s up!'));
   console.log();
-  console.log(chalk.gray(wrap('This generator saves time by offering to re-use answers from the previous run. If something is incorrect, no worries! Just provide a new value!')));
+  console.log(utils.chalk.bold('  Hi there!'));
+  console.log();
+  console.log(utils.wrap('Looks like your first time running the "node" generator. '));
+  console.log();
+  console.log(utils.wrap('This generator saves time by offering to re-use answers from the previous run. If something is incorrect, no worries! Just provide a new value!'));
   console.log();
 }
---
+++ utils.js
@@ -0,0 +1,47 @@
+'use strict';
+
+/**
+ * Module dependencies
+ */
+
+var utils = require('lazy-cache')(require);
+
+/**
+ * Temporarily re-assign `require` to trick browserify and
+ * webpack into reconizing lazy dependencies.
+ *
+ * This tiny bit of ugliness has the huge dual advantage of
+ * only loading modules that are actually called at some
+ * point in the lifecycle of the application, whilst also
+ * allowing browserify and webpack to find modules that
+ * are depended on but never actually called.
+ */
+
+var fn = require;
+require = utils;
+
+/**
+ * Lazily required module dependencies
+ */
+
+require('extend-shallow', 'extend');
+require('question-store', 'questions');
+require('through2', 'through');
+require('word-wrap', 'wrap');
+require('set-value', 'set');
+require('project-name');
+require('namify');
+require('minimist');
+require('chalk');
+
+/**
+ * Restore `require`
+ */
+
+require = fn;
+
+/**
+ * Expose `utils` modules
+ */
+
+module.exports = utils;
